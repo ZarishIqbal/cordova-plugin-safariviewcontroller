6ee6f91a1f85aefb178620fd1141f213f817337b
zarishiqbal <zarishiqbal.zi@gmail.com>
2024-01-08 12:45:40 +0300

    adding ui components to webkit

--- src/ios/SafariViewController.h
+++ src/ios/SafariViewController.h
@@ -6,6 +6,15 @@
 @interface SafariViewController : CDVPlugin <WKNavigationDelegate>
 
 @property (nonatomic, copy) NSString* callbackId;
+@property (nonatomic, copy) NSString* URLString;
+
+@property (weak, nonatomic) IBOutlet UIView *barView;
+@property (weak, nonatomic) IBOutlet UITextField *urlField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *reloadButton;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+@property (strong, nonatomic) WKWebView *webView;
 
 - (void) isAvailable:(CDVInvokedUrlCommand*)command;
 - (void) show:(CDVInvokedUrlCommand*)command;
--- src/ios/SafariViewController.m
+++ src/ios/SafariViewController.m
@@ -82,11 +82,72 @@ if (webView.superview != nil) {
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+
+- (IBAction)back:(UIBarButtonItem *)sender {
+    if ([self.webView canGoBack]) {
+        [self.webView goBack];
+    }
+}
+
+- (IBAction)forward:(UIBarButtonItem *)sender {
+    if ([self.webView canGoForward]) {
+        [self.webView goForward];
+    }
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    self.barView.frame = CGRectMake(0, 0, size.width, 30);
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.urlField resignFirstResponder];
+    NSURL *url = [NSURL URLWithString:self.urlField.text];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    WKBackForwardList *list = [self.webView backForwardList];
+    NSLog(@"BACK LIST");
+    NSLog(@"%@", list.backList);
+    return NO;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"loading"]) {
+        self.backButton.enabled = self.webView.canGoBack;
+        self.forwardButton.enabled = self.webView.canGoForward;
+    }
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progressView.hidden = self.webView.estimatedProgress == 1;
+        [self.progressView setProgress:self.webView.estimatedProgress animated:YES];
+    }
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.barView.frame = CGRectMake(0, 0, self.view.frame.size.width, 30);
+    
+    [self.view insertSubview:self.webView belowSubview:self.progressView];
+    
+    self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1 constant:-44];
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.webView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
+    [self.view addConstraints:@[height, width]];
+    
+    self.backButton.enabled = NO;
+    self.forwardButton.enabled = NO;
+    
+    [self.webView addObserver:self forKeyPath:@"loading" options:NSKeyValueObservingOptionNew context:NULL];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:NULL];
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.appcoda.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
 #pragma mark - WKNavigationDelegate
 
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
     // This method is called when the web view starts loading a new frame.
-     CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"event":@"started"}];
+     CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"event":@"started",@"url": webView.URL.absoluteString}];
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 
