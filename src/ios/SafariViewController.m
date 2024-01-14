de2807c6270154ae6f5358731575992a8dcd0dc4
zarishiqbal <zarishiqbal.zi@gmail.com>
2024-01-14 10:52:55 +0300

    bug fix

--- src/ios/SafariViewController.m
+++ src/ios/SafariViewController.m
@@ -6,15 +6,27 @@
 }
 
 //initialize UIWebViewController
-- (void)pluginInitialize:(CDVInvokedUrlCommand*)command {
+- (void)pluginInitialize(CDVInvokedUrlCommand*)command {
    wc = [[UIWebViewController alloc] initWithCallbackId:command.callbackId withDelegate:self.commandDelegate];
 }
 
 //check if UIWebViewController is available
 - (void)isAvailable:(CDVInvokedUrlCommand*)command {
     BOOL available = [wc isAvailable];
+        // if available, return true
+        // if not available, return false
+        if (available) {
+    // Code to execute if available is true
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:available];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+} else {
+    // Code to execute if available is false
+     wc = [[UIWebViewController alloc] initWithCallbackId:command.callbackId withDelegate:self.commandDelegate];
+      BOOL available_now = [wc isAvailable];
+       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:available_wc];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+   
 }
 
 //open SafariViewController
